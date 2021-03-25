e1d05b688ad42d989371c482dbd77db0be282020
Michel Zarzosa Rojas <michel.zaro@gmail.com>
2021-03-25 15:56:33 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+
+int handle_infix(char *line) {
+    return 0;
+}
+
+int handle_postfix(char *line) {
+    return 0;
+}
+
+int handle_prefix(char *line) {
+    return 0;
+}
+
+bool is_infix(char *line) {
+    return true;
+}
 
 int main()
 {
-    printf("Hello World");
+    char test[10];
+    printf("introduce la operación: \n");
+    scanf("%s",&test);
 
+    for (int i=0; ; i++) {
+        if (test[i] == '\0')
+            break;
+        switch (test[i]) {
+            case ' ':
+                continue;
+            case '\t':
+                continue;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                handle_prefix(test);
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            if (is_infix(test)) {
+                handle_infix(test);
+            }
+            else {
+                handle_postfix(test);
+            }
+            break;
+
+        }
+        printf("%c\n", test[i]);
+    }
+    
     return 0;
 }
